fe20df9c052a3a8c3adb9acdb3c21b91388b2528
Shady Gara <shadygara@h-MacBook-Pro-sl-Shady.local>
2024-12-12 20:44:35 +0200

    scrabble game

--- Scrabble.java
+++ Scrabble.java
@@ -1,3 +1,4 @@
+
 /*
  * RUNI version of the Scrabble game.
  */
@@ -48,7 +49,10 @@ public class Scrabble {
 
 	// Checks if the given word is in the dictionary.
 	public static boolean isWordInDictionary(String word) {
-		//// Replace the following statement with your code
+		for (int i = 0; i < NUM_OF_WORDS; i++)
+			if (DICTIONARY[i].equals(word)) {
+				return true;
+			}
 		return false;
 	}
 	
@@ -56,19 +60,39 @@ public class Scrabble {
 	// If the length of the word equals the length of the hand, adds 50 points to the score.
 	// If the word includes the sequence "runi", adds 1000 points to the game.
 	public static int wordScore(String word) {
+		int score = 0;
+		if (word.isEmpty() || !isWordInDictionary(word)) {
+			return 0;
+		}
+		else {
+			for (int i = 0; i < word.length(); i++) {
+				char currentCh = word.charAt(i);
+				int letterValue = currentCh - 'a';
+				score += SCRABBLE_LETTER_VALUES[letterValue];
+			}
+		}
+		int finalScore = score * word.length();
 
-		return 0;
+		if (MyString.subsetOf("runi", word)) {
+			finalScore += 1000;
+		}
+		if (word.length() == HAND_SIZE) {
+			finalScore += 50;
+		}
+		return finalScore;
 	}
 
 	// Creates a random hand of length (HAND_SIZE - 2) and then inserts
 	// into it, at random indexes, the letters 'a' and 'e'
 	// (these two vowels make it easier for the user to construct words)
 	public static String createHand() {
-		int hand = HAND_SIZE - 2;
-		 
-		MyString.insertRandomly('a', );
+		int handWithoutVowels = HAND_SIZE - 2;
 
-		return null;
+		String randomLetters = MyString.randomStringOfLetters(handWithoutVowels);
+		String handWithVowel1 = MyString.insertRandomly('a', randomLetters);
+		handWithVowel1 = MyString.insertRandomly('e', handWithVowel1);
+
+		return handWithVowel1;
 	}
 	
     // Runs a single hand in a Scrabble game. Each time the user enters a valid word:
@@ -76,7 +100,6 @@ public class Scrabble {
     // 2. The user gets the Scrabble points of the entered word.
     // 3. The user is prompted to enter another word, or '.' to end the hand. 
 	public static void playHand(String hand) {
-		int n = hand.length();
 		int score = 0;
 		// Declares the variable in to refer to an object of type In, and initializes it to represent
 		// the stream of characters coming from the keyboard. Used for reading the user's inputs.   
@@ -88,9 +111,19 @@ public class Scrabble {
 			// non-whitespace characters. Whitespace is either space characters, or  
 			// end-of-line characters.
 			String input = in.readString();
-			//// Replace the following break statement with code
-			//// that completes the hand playing loop
-			break;
+			if (input.equals(".")) {
+				break;
+			}
+			else {
+				if (isWordInDictionary(input)) {
+					hand = MyString.remove(hand, input);
+					score += wordScore(input);
+					System.out.println(input + " earned " + wordScore(input) + " points. " + "Score: " + score + " points");
+				}
+				else {
+					System.out.println("No such word in the dictionary. Try again.");
+				}
+			}
 		}
 		if (hand.length() == 0) {
 	        System.out.println("Ran out of letters. Total score: " + score + " points");
@@ -113,9 +146,13 @@ public class Scrabble {
 			// Gets the user's input, which is all the characters entered by 
 			// the user until the user enter the ENTER character.
 			String input = in.readString();
-			//// Replace the following break statement with code
-			//// that completes the game playing loop
-			break;
+			if (input.equals("e")) {
+				break;
+			}
+			else {
+				String currentHand = createHand();
+				playHand(currentHand);
+			}
 		}
 	}
 
@@ -123,9 +160,9 @@ public class Scrabble {
 		//// Uncomment the test you want to run
 		////testBuildingTheDictionary();  
 		////testScrabbleScore();    
-		////testCreateHands();  
-		////testPlayHands();
-		////playGame();
+		//testCreateHands();  
+		//testPlayHands();
+		playGame();
 	}
 
 	public static void testBuildingTheDictionary() {
